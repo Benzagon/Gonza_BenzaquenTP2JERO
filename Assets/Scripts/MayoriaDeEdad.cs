6ac6c6575eb10668459d6e836eb14c2ac5af9abb
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-04-06 15:37:01 -0300

    Videos 1 y 2

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsusario;
+
+    void Start()
+    {
+        if(edadUsusario >= 10)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
