64b7ec8fb993a140637ca69f0652d760cca6fa34
Benzagon <gonbenzaquen@gmail.com>
2022-04-11 21:04:55 -0300

    Tp_Terminado

--- Assets/Scripts/MayoriaDeEdad.cs
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MayoriaDeEdad : MonoBehaviour
 {
     public int edadUsusario;
+    public Text miTexto;
 
     void Start()
     {
         if(edadUsusario >= 10)
         {
             Debug.Log("Es mayor de edad");
+            miTexto.text = "Es mayor de edad";
         }
         else
         {
             Debug.Log("No es mayor de edad");
+            miTexto.text = "No es mayor de edad";
         }
     }
 
---
+++ Assets/Scripts/ValidarContra.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContra : MonoBehaviour
+{
+    string contraseniaCorrecta, contraseniaUsr;
+    public Text ingresoUsr;
+    public Text txtMsj;
+    public GameObject cartelMsj;
+
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelMsj.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void validarContra()
+    {
+        contraseniaUsr = ingresoUsr.text;
+
+        if(contraseniaUsr == contraseniaCorrecta)
+        {
+            txtMsj.text = "¡Bienvenido!";
+            cartelMsj.SetActive(true);
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            txtMsj.text = "Contraseña Incorrecta";
+            cartelMsj.SetActive(true);
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
